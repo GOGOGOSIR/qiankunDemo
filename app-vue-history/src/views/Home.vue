c710d2ece1978142030ae9e11d506140f26c5bae
wanjianqiang <17688728066@163.com>
2021-02-01 13:17:07 +0800

    feat: init project

---
+++ app-vue-history/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <router-link to="/app-vue-hash/about">hash about</router-link>
+
+    <router-link to="/Agent/MyHouse">MyHouse</router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
